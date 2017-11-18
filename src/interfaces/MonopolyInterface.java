f56ece56837c4e389284efbd26d817cfa2ef8785
Ricardo Alcalá <scroff97@gmail.com>
2017-11-18 13:24:44 -0500

    Creando la interfaz

---
+++ src/interfaces/MonopolyInterface.java
@@ -0,0 +1,20 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package interfaces;
+
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+/**
+ *
+ * @author Flia Alcalá Castro
+ */
+public interface MonopolyInterface extends Remote {
+    
+    public boolean logIn(String name)throws RemoteException;
+    public boolean logOut(String name) throws RemoteException;
+    
+}
